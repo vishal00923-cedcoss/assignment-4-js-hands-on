8e92d6f4b3c1966821396cd3fc35a5c85976b663
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 13:52:07 +0530

    Comparison with the Strict Inequality Operator

--- src/script.js
+++ src/script.js
@@ -408,3 +408,13 @@ function testNotEqual(val) {
 
 // Change this value to test
 testNotEqual(10);
+
+function testStrictNotEqual(val) {
+  if (val !== 17) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
