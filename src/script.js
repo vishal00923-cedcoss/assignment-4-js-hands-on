76a66fb5d315672b2abdac6fdc44af7c2e27b382
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 11:34:17 +0530

    Add Two Numbers with JavaScript

--- src/script.js
+++ src/script.js
@@ -54,3 +54,5 @@ const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
+
+const sum = 10 + 10;
