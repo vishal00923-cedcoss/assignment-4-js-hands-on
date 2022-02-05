429fdd06f4c03de2543019596f9168fe3fb4e821
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 14:20:50 +0530

    Chaining If Else Statements

--- src/script.js
+++ src/script.js
@@ -526,3 +526,19 @@ function orderMyLogic(val) {
     return "Greater than or equal to 10";
   }
 }
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+  // Only change code above this line
+}
