6a92ca08b644afbf0a419ecd341648e9f002c89f
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 14:52:44 +0530

    Accessing Object Properties with Variables

--- src/script.js
+++ src/script.js
@@ -747,3 +747,15 @@ const testObj = {
 
 const entreeValue = testObj["an entree"]; // Change this line
 const drinkValue = testObj["the drink"]; // Change this line
+
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas",
+};
+
+// Only change code below this line;
+
+const playerNumber = 16; // Change this Line
+const player = testObj[playerNumber]; // Change this Line
