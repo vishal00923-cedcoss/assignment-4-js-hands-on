6d972edb979d4b3603f50e411ae1465a2d7e1bf5
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 14:39:54 +0530

    Return Early Pattern for Functions

--- src/script.js
+++ src/script.js
@@ -669,3 +669,18 @@ function isLess(a, b) {
 }
 // Change these values to test
 isLess(10, 15);
+
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2, 2);
