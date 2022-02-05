a57f56b2b744ff6102d6d8d2cbf183facab0cbd1
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 12:00:31 +0530

    Compound Assignment With Augmented Addition

--- src/script.js
+++ src/script.js
@@ -84,3 +84,12 @@ const product = 5.0 * 1;
 const quotient = 4.4 / 2.0; // Change this line
 
 const remainder = 5 % 3;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
