1ba1756170f16860174ec3f1042b61ce97e7ff93
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 11:16:36 +0530

    Understanding Uninitialized Variables

--- src/script.js
+++ src/script.js
@@ -23,3 +23,16 @@ var a = 9;
 
 var myFirstName = "Vishal";
 var myLastName = "Chaurasia";
+
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5;
+b = 10;
+c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
