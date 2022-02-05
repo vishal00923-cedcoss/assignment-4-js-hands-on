6b36e316e98173d77d77e86e167d785006bc33a6
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 12:39:52 +0530

    Use Bracket Notation to Find the First Character in a String

--- src/script.js
+++ src/script.js
@@ -148,3 +148,10 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 lastNameLength = lastName.length;
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
