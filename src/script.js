4bb8892aacdef79b9d9281274e4b97ebf7bcbb52
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 12:27:54 +0530

    Concatenating Strings with Plus Operator

--- src/script.js
+++ src/script.js
@@ -126,3 +126,5 @@ const myStr = 'I am a "double quoted" string inside "double quotes".';
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+const myStr = "This is the start." + " This is the end."; // Change this line
