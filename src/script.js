787d729fda0041d1a729c61ac962fe9e93d87d0a
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 13:04:00 +0530

    Manipulate Arrays With push()

--- src/script.js
+++ src/script.js
@@ -208,3 +208,13 @@ const myArray = [
 ];
 
 const myData = myArray[2][1];
+
+// Setup
+const myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+// Only change code below this line
+
+myArray.push(["dog", 3]);
