bd6b9ed87c1f311b048e0ae35ed917530a81f6a1
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 13:47:29 +0530

    Practice comparing different values

--- src/script.js
+++ src/script.js
@@ -384,3 +384,15 @@ function testStrict(val) {
 
 // Change this value to test
 testStrict(10);
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
