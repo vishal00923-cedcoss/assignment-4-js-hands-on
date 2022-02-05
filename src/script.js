4a8583c66851df3b6e9ab5f4b3f4a7093af89464
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 13:59:42 +0530

    Comparison with the Greater Than Or Equal To Operator

--- src/script.js
+++ src/script.js
@@ -435,3 +435,11 @@ function testGreaterThan(val) {
 
 // Change this value to test
 testGreaterThan(10);
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) return "20 or Over";
+
+  if (val >= 10) return "10 or Over";
+
+  return "Less than 10";
+}
