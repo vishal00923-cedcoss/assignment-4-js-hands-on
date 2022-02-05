1ae478a4a439fee5277a010be91cd0fdc4343380
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 14:10:14 +0530

    Comparisons with the Logical Or Operator

--- src/script.js
+++ src/script.js
@@ -473,3 +473,16 @@ function testLogicalAnd(val) {
 
 // Change this value to test
 testLogicalAnd(10);
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
