db8664869811d49955dd571eb338790f1bc47d24
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 14:48:28 +0530

    Accessing Object Properties with Dot Notation

--- src/script.js
+++ src/script.js
@@ -725,3 +725,14 @@ const myDog = {
   tails: 1,
   friends: ["Water", "Dogs"],
 };
+
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats",
+};
+
+// Only change code below this line
+
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
