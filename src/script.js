f30486ad626894f0d9ca44d86881939f45bfbbe2
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 15:10:57 +0530

    Testing Objects for Properties

--- src/script.js
+++ src/script.js
@@ -811,3 +811,13 @@ function phoneticLookup(val) {
   // Only change code above this line
   return result;
 }
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+  // Only change code above this line
+}
