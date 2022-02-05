fe28c618906fd059a0090f57f38ba7231aa5aac9
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-05 14:51:00 +0530

    Accessing Object Properties with Bracket Notation

--- src/script.js
+++ src/script.js
@@ -736,3 +736,14 @@ const testObj = {
 
 const hatValue = testObj.hat; // Change this line
 const shirtValue = testObj.shirt; // Change this line
+
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water",
+};
+
+// Only change code below this line
+
+const entreeValue = testObj["an entree"]; // Change this line
+const drinkValue = testObj["the drink"]; // Change this line
