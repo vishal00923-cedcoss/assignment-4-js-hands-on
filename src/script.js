b94b08aa9c245e80d73ee5cf257a37422e193c6d
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-07 09:45:07 +0530

    Count Backwards With a For Loop

--- src/script.js
+++ src/script.js
@@ -930,3 +930,12 @@ const myArray = [];
 for (let i = 1; i <= 9; i += 2) {
   myArray.push(i);
 }
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 9; i >= 1; i -= 2) {
+  myArray.push(i);
+}
